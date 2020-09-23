b7f333be0ac1598770470fc3e5fff7fa321bfbfc
attarizi <42281208+attarizi@users.noreply.github.com>
2020-09-23 10:32:04 +0200

    replace Calculator test functions

--- src/shop/ParserTest.java
+++ src/shop/ParserTest.java
@@ -28,4 +28,10 @@ public class ParserTest {
 		assertEquals(parser2.getCountOfD(), 1);
 	}
 
+	@Test
+	public void create_calculator_test() {
+		Calculator calculator = new Calculator(5, 5, 5, 5);
+		assertNotNull(calculator);
+	}
+
 }
