8d9400c3238f317d05002d6b11a02d27c07d9648
attarizi <42281208+attarizi@users.noreply.github.com>
2020-09-23 20:13:30 +0200

    UI improvement

--- src/shop/ShopMainClass.java
+++ src/shop/ShopMainClass.java
@@ -1,13 +1,17 @@
 package shop;
 
+import java.util.Scanner;
+
 public class ShopMainClass {
 
 	public static void main(String[] args) {
-		Parser parser = new Parser(args[0]);
+		System.out.println("Enter an input: ");
+		String input = new Scanner(System.in).nextLine();
+
+		Parser parser = new Parser(input);
 		Calculator calculator = new Calculator(parser.getCountOfA(), parser.getCountOfB(), parser.getCountOfC(),
 				parser.getCountOfD());
-		System.out.println(calculator.printResult());
+		System.out.println("Result : " + calculator.printResult());
 
 	}
-
 }
