b7cc0a210a62df265be7dfdc7953d450a264dc84
attarizi <42281208+attarizi@users.noreply.github.com>
2020-09-23 10:08:33 +0200

    create counter test and counter functions in Parser

---
+++ src/shop/Parser.java
@@ -0,0 +1,29 @@
+package shop;
+
+public class Parser {
+
+	public Parser(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCountOfA() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCountOfB() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCountOfC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getCountOfD() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/shop/ParserTest.java
+++ src/shop/ParserTest.java
@@ -1,5 +1,6 @@
 package shop;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -8,8 +9,18 @@ public class ParserTest {
 
 	@Test
 	public void create_test() {
-		Parser parser = new Parser();
+		Parser parser = new Parser("");
 		assertNotNull(parser);
+	}
+
+	@Test
+	public void parser_counter_test() {
+		Parser parser = new Parser("AAABBBCD");
+
+		assertEquals(parser.getCountOfA(), 3);
+		assertEquals(parser.getCountOfB(), 3);
+		assertEquals(parser.getCountOfC(), 1);
+		assertEquals(parser.getCountOfD(), 1);
 	}
 
 }
