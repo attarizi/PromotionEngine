955622990aae4dc9ec8cafa0b34df6e972d1a3ae
attarizi <42281208+attarizi@users.noreply.github.com>
2020-09-23 11:12:02 +0200

    Add main class to the project

---
+++ src/shop/ShopMainClass.java
@@ -0,0 +1,13 @@
+package shop;
+
+public class ShopMainClass {
+
+	public static void main(String[] args) {
+		Parser parser = new Parser(args[0]);
+		Calculator calculator = new Calculator(parser.getCountOfA(), parser.getCountOfB(), parser.getCountOfC(),
+				parser.getCountOfD());
+		System.out.println(calculator.calc());
+
+	}
+
+}
